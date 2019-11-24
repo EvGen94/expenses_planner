0a351a82be19a104028100c88c117c4837e745e9
EvGen94 <sharanov.evgeniigoogl@gmail.com>
2019-11-24 17:44:27 +0300

    add_transaction

--- expenses_planner/lib/main.dart
+++ expenses_planner/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,26 +14,42 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'new_Game',
+      amount: 899.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Metro',
+      amount: 150,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Container(width: 1000,color: Colors.greenAccent,
-        child: Column(
-          children: <Widget>[
-            Card(
-              color: Colors.amber[300],
-              child: Container(width: 100, child: Text('CHART!')),
-              elevation: 10,
-            ),
-            Card(
-              color:  Colors.deepOrangeAccent[300],
-              child: Container(width: 100, child: Text('List of txt')),
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Card(
+            color: Colors.amber[300],
+            child: Container(width: 200, child: Text('CHART!')),
+            elevation: 10,
+          ),
+          Card(
+            color: Colors.deepOrangeAccent[300],
+            child: Container(width: 200, child: Text('List of txt')),
+            elevation: 10,
+          ),
+        ],
       ),
     );
   }
